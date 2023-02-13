6af85cc50be0ce924ca2ee02c6f113c972fc2e67
mooolch <112543907+mooolch@users.noreply.github.com>
2023-02-13 21:27:51 +0200

    Add files via upload

---
+++ lesson-2/js/task-4.js
@@ -0,0 +1,18 @@
+// Декларуємо константи
+const CENTIMETR_TO_METR_RATIO = 0.01;
+const CENTIMETR_TO_KILOMETR_RATIO = 0.00001;
+
+// Робимо запит довжини елементу у користувача
+let centimetrs = parseFloat(prompt('Введіть довжину елементу в см', '0'));
+
+//Робимо підрахунки
+let resultInMetrs = centimetrs * CENTIMETR_TO_METR_RATIO;
+let resultInKilometrs = centimetrs * CENTIMETR_TO_KILOMETR_RATIO;
+
+// Виводимо результат
+let el = document.querySelector('.task__body');
+el.innerHTML = `
+<p>Довжина елементу дорівнює ${resultInMetrs} м.</p>
+<p>Довжина елементу дорівнює ${resultInKilometrs} км.</p>
+`;
+``
