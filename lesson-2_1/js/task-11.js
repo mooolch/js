b3f10da055efd18d31441cc5fb76157c269ed40a
mooolch <112543907+mooolch@users.noreply.github.com>
2023-02-15 22:58:26 +0200

    Delete lesson-2_1 directory

--- lesson-2_1/js/task-11.js
+++
@@ -1,8 +0,0 @@
-const daysQuantity = parseInt(prompt('Введіть кількість днів', '7'));
-
-const DAYS_IN_A_WEEK = 7;
-
-// Вираховуємо кількість повних тижнів
-const wholeWeeksNumber = Math.floor(daysQuantity / DAYS_IN_A_WEEK);
-
-console.log(`У ${daysQuantity} днях - ${wholeWeeksNumber} повних тижнів.`);
