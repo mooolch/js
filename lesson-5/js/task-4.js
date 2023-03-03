36beac56d3187e41c49daea5567a8850296dea2f
mooolch <112543907+mooolch@users.noreply.github.com>
2023-03-03 13:52:35 +0200

    Update task-4.js

--- lesson-5/js/task-4.js
+++ lesson-5/js/task-4.js
@@ -7,15 +7,15 @@ const num3 = parseFloat(prompt('Введіть третє число', '44'))
 
 //2. Считаем результат
 function calcResults() {
-	let oddNumbers = 0
+	let evenNumbers = 0
 	let positiveNumbers = 0
 	let moreThatAHundredNums = 0
 	for (let i = 0; i < arguments.length; i++) {
-		if (arguments[i] % 2 === 0) oddNumbers++
+		if (arguments[i] % 2 === 0) evenNumbers++
 		if (arguments[i] > 0) positiveNumbers++
 		if (arguments[i] > 100) moreThatAHundredNums++
 	}
-	let result = `Кількість:\nПарних - ${oddNumbers}.\nПозитивних - ${positiveNumbers}.\nБільших за 100 - ${moreThatAHundredNums}.`
+	let result = `Кількість:\nПарних - ${evenNumbers}.\nПозитивних - ${positiveNumbers}.\nБільших за 100 - ${moreThatAHundredNums}.`
 	return result
 }
 
